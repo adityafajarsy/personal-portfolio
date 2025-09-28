23401bdae3c31409c9e1d96349d0d4823a411f60
adityafajarsy <adityafajar.sy90@gmail.com>
2025-09-28 09:02:24 +0700

    first initialize

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center'>
+      <h1 className='text-2xl font-black'>Portfolio</h1>
+      <div className='flex gap-7'>
+        <a href="#beranda">Beranda</a>
+        <a href="#tentang">Tentang</a>
+        <a href="#project">Project</a>
+      </div>
+
+      <div className='flex items-center gap-3'>
+        <a href="#">
+            <i className='ri-github-fill ri-2x'></i>
+        </a>
+        <a href="#">
+            <i className='ri-instagram-fill ri-2x'></i>
+        </a>
+        <a href="#">
+            <i className='ri-linkedin-fill ri-2x'></i>
+        </a>
+        <a href="#">
+            <i className='ri-youtube-fill ri-2x'></i>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
